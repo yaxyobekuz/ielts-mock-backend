984ca38b030c753639b0aafb90dd735e3e596557
yaxyobekuz <pubgn9642@gmail.com>
2025-09-01 14:28:47 +0500

    Add verificationCodeSent text template

---
+++ src/data/texts.js
@@ -0,0 +1,6 @@
+const texts = {
+  verificationCodeSent: (code) =>
+    `<b>Hisobingizni tasdiqlash kodingiz:</b> <code>${code}</code>\nUshbu kodni hech kimga bermang, Hatto IELTS admini so'rasa ham!`,
+};
+
+module.exports = texts;

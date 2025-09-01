80182d6b8ae7488e2c98a3aed0809223754831f0
yaxyobekuz <pubgn9642@gmail.com>
2025-09-01 14:27:18 +0500

    Add getRandomNumber helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
 const delay = (ms = 1000) => new Promise((res) => setTimeout(res, ms));
 
-module.exports = { delay };
+const getRandomNumber = (min = 0, max = 1) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports = { delay, getRandomNumber };

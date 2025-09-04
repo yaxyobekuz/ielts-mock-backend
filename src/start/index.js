f4e49b2e35601fc9e1e8fb7717beb8e8c23b6bcf
yaxyobekuz <pubgn9642@gmail.com>
2025-09-04 13:32:52 +0500

    Register models in start/index for initialization

--- src/start/index.js
+++ src/start/index.js
@@ -1,6 +1,13 @@
 const cors = require("cors");
 const express = require("express");
 
+// Models
+require("../models/Part");
+require("../models/User");
+require("../models/Test");
+require("../models/Section");
+require("../models/VerificationCode");
+
 const app = express();
 app.use(cors());
 

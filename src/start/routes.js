8e7d475940d5e2c81eaa26dc5d13ce6346f83884
yaxyobekuz <pubgn9642@gmail.com>
2025-09-05 11:43:25 +0500

    Register parts and sections routes in app

--- src/start/routes.js
+++ src/start/routes.js
@@ -3,7 +3,11 @@ const { app, express } = require("./index");
 // Routes
 const authRoute = require("../routes/auth");
 const testsRoute = require("../routes/tests");
+const partsRoute = require("../routes/parts");
+const sectionsRoute = require("../routes/sections");
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/tests", testsRoute);
+app.use("/api/parts", partsRoute);
+app.use("/api/sections", sectionsRoute);

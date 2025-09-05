eaf45a9dbdff24d77c119afacf22fcaa6653c0eb
yaxyobekuz <pubgn9642@gmail.com>
2025-09-05 11:34:13 +0500

    Add module, testId & partId properties to Section model

--- src/models/Section.js
+++ src/models/Section.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const AnswerSchema = new mongoose.Schema({ text: { type: String } });
+const Answer = new mongoose.Schema({ text: { type: String } });
 
-const GroupSchema = new mongoose.Schema({
-  answers: [AnswerSchema],
+const Group = new mongoose.Schema({
+  answers: [Answer],
   question: { type: String, required: true },
   correctAnswerIndex: { type: Number, required: true },
 });
 
-const OptionSchema = new mongoose.Schema({
+const Option = new mongoose.Schema({
   option: { type: String, required: true },
 });
 
@@ -17,6 +17,9 @@ const Section = new mongoose.Schema(
     // Base
     description: { type: String },
     title: { type: String, required: true },
+    module: { type: String, required: true },
+    partId: { type: String, required: true },
+    testId: { type: String, required: true },
     questionsCount: { type: Number, default: 0, required: true },
     type: {
       type: String,
@@ -26,13 +29,13 @@ const Section = new mongoose.Schema(
 
     // text
     text: { type: String },
-    answers: [AnswerSchema],
+    answers: [Answer],
 
     // radio-group
-    groups: [GroupSchema],
+    groups: [Group],
 
     // text-draggable & flowchart
-    options: { title: { type: String }, data: [OptionSchema] },
+    options: { title: { type: String }, data: [Option] },
 
     // flowchart
     items: { title: { type: String }, data: [{ text: { type: String } }] },

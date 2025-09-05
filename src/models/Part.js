898d82ddf814625bab1947c158c51c5fdf24b0cf
yaxyobekuz <pubgn9642@gmail.com>
2025-09-05 11:33:46 +0500

    Add module & testId properties to Part model

--- src/models/Part.js
+++ src/models/Part.js
@@ -4,7 +4,9 @@ const Part = new mongoose.Schema(
   {
     description: { type: String },
     number: { type: Number, required: true },
-    totalQuestions: { type: Number, required: true },
+    module: { type: String, required: true },
+    testId: { type: String, required: true },
+    totalQuestions: { type: Number, required: true, default: 0 },
     sections: [{ type: mongoose.Schema.Types.ObjectId, ref: "Section" }],
     createdBy: {
       ref: "User",

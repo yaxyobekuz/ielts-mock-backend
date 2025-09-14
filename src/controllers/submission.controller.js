f693475f083d509ea6cc0b35a9deda28d58ec747
yaxyobekuz <pubgn9642@gmail.com>
2025-09-14 11:56:06 +0500

    Add submission controller

---
+++ src/controllers/submission.controller.js
@@ -0,0 +1,113 @@
+// Models
+const Link = require("../models/Link");
+const Test = require("../models/Test");
+const Submission = require("../models/Submission");
+
+// Yangi submission yaratish
+const createSubmission = async (req, res, next) => {
+  const { linkId, answers, userInfo } = req.body;
+
+  try {
+    // Link
+    const link = await Link.findById(linkId);
+    if (!link) {
+      return res.status(404).json({
+        code: "linkNotFound",
+        message: "Havola topilmadi",
+      });
+    }
+
+    // Test
+    const test = await Test.findById(link.testId);
+    if (!test) {
+      return res.status(404).json({
+        code: "testNotFound",
+        message: "Test topilmadi",
+      });
+    }
+
+    const submission = await Submission.create({
+      answers,
+      userInfo,
+      link: linkId,
+      test: link.testId,
+      finishedAt: Date.now(),
+      teacher: link.createdBy,
+    });
+
+    res.status(201).json({
+      code: "submissionCreated",
+      message: "Test javoblari qabul qilindi",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Barcha submissionlarni olish
+const getSubmissions = async (req, res, next) => {
+  try {
+    const submissions = await Submission.find().populate(
+      "test link user teacher"
+    );
+    res.json(submissions);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// ID bo‘yicha submission olish
+const getSubmissionById = async (req, res, next) => {
+  try {
+    const submission = await Submission.findById(req.params.id).populate(
+      "test link user teacher"
+    );
+    if (!submission)
+      return res.status(404).json({ message: "Submission topilmadi" });
+    res.json(submission);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Submission yangilash (masalan, foydalanuvchi javob qo‘shsa yoki testni tugatsa)
+const updateSubmission = async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    // Agar test tugasa finishedAt qo‘shiladi
+    if (data.isFinished) {
+      data.finishedAt = new Date();
+    }
+
+    const submission = await Submission.findByIdAndUpdate(req.params.id, data, {
+      new: true,
+    });
+    if (!submission)
+      return res.status(404).json({ message: "Submission topilmadi" });
+
+    res.json(submission);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Submission o‘chirish
+const deleteSubmission = async (req, res, next) => {
+  try {
+    const submission = await Submission.findByIdAndDelete(req.params.id);
+    if (!submission)
+      return res.status(404).json({ message: "Submission topilmadi" });
+    res.json({ message: "Submission o‘chirildi" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getSubmissions,
+  createSubmission,
+  updateSubmission,
+  deleteSubmission,
+  getSubmissionById,
+};

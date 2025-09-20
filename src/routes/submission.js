0d56992e50c33ce416ca3cb2f3cc66097dbcc18e
yaxyobekuz <pubgn9642@gmail.com>
2025-09-20 13:32:45 +0500

    Add auth & roleCheck middlewares to submission routes

--- src/routes/submission.js
+++ src/routes/submission.js
@@ -2,9 +2,6 @@
 const express = require("express");
 const router = express.Router();
 
-// Middlewares
-const { auth } = require("../middlewares/auth");
-
 // Controllers
 const {
   getSubmissions,
@@ -14,10 +11,14 @@ const {
   getSubmissionById,
 } = require("../controllers/submission.controller");
 
+// Middlewares
+const { auth, roleCheck } = require("../middlewares/auth");
+const notStudent = roleCheck(["teacher", "supervisor", "owner", "admin"]);
+
 router.get("/", auth, getSubmissions);
-router.post("/", auth, createSubmission);
-router.put("/:id", auth, updateSubmission);
 router.get("/:id", auth, getSubmissionById);
-router.delete("/:id", auth, deleteSubmission);
+router.post("/", auth, notStudent, createSubmission);
+router.put("/:id", auth, notStudent, updateSubmission);
+router.delete("/:id", auth, notStudent, deleteSubmission);
 
 module.exports = router;

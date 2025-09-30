ca4fb5d37530be445947b2bc4f7200fcb0a84981
yaxyobekuz <pubgn9642@gmail.com>
2025-09-30 10:24:39 +0500

    Add teacher controller with create, fetch, update, and get by ID functions

---
+++ src/controllers/teachers.controller.js
@@ -0,0 +1,165 @@
+// Models
+const User = require("../models/User");
+const Teacher = require("../models/User");
+
+// Helpers
+const { getRandomNumber, extractNumbers } = require("../utils/helpers");
+
+// Create teacher
+const createTeacher = async (req, res, next) => {
+  const supervisorId = req.user._id;
+  const { phone, password } = req.body;
+
+  const formattedPassword = password?.trim() || "";
+  const formattedPhone = extractNumbers(phone) || "";
+
+  if (formattedPhone.length !== 12) {
+    return res.status(400).json({
+      code: "invalidPhone",
+      message: "Telefon raqam noto'g'ri",
+    });
+  }
+
+  if (formattedPassword.length < 4) {
+    return res.status(400).json({
+      code: "veryShortPassword",
+      message: "Parol juda ham qisqa",
+    });
+  }
+
+  try {
+    // If teacher exists
+    let teacher = await User.findOne({ phone });
+    if (teacher && teacher.supervisor === supervisorId) {
+      return res.status(400).json({
+        code: "teacherAlreadyCreated",
+        message: "Ustoz allaqachon qo'shilgan",
+      });
+    }
+
+    // If teacher exists
+    if (teacher) {
+      return res.status(400).json({
+        code: "phoneAlreadyUsed",
+        message: "Telefon raqam allaqachon ishlatilgan",
+      });
+    }
+
+    // If teacher does not exist, create new one
+    teacher = await User.create({
+      role: "teacher",
+      firstName: "Ustoz",
+      supervisor: supervisorId,
+      password: formattedPassword,
+      phone: Number(formattedPhone),
+      lastName: `#${getRandomNumber(0, 99)}`,
+    });
+
+    res.status(201).json({
+      teacher,
+      code: "teacherCreated",
+      message: "Yangi ustoz muvaffaqiyatli yaratildi",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get teachers
+const getTeachers = async (req, res, next) => {
+  const { _id: supervisorId, role: userRole } = req.user;
+
+  try {
+    let filter = {};
+    if (userRole === "supervisor") filter.supervisor = supervisorId;
+
+    const teachers = await User.find(filter)
+      .select("-__v -password -balance -chatId")
+      .sort({ createdAt: -1 });
+
+    res.json({
+      teachers,
+      code: "teachersFetched",
+      message: "Ustozlar muvaffaqiyatli olindi",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get single teacher by ID
+const getTeacherById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const teacher = await Teacher.findById(id)
+      .populate("createdBy", "firstName lastName role avatar")
+      .populate({
+        path: "listening.parts reading.parts writing.parts",
+        populate: { path: "sections", model: "Section" },
+      })
+      .select("-__v");
+
+    if (!teacher) {
+      return res
+        .status(404)
+        .json({ code: "teacherNotFound", message: "Teacher topilmadi" });
+    }
+
+    res.json({
+      teacher,
+      code: "teacherFetched",
+      message: "Teacher muvaffaqiyatli olindi",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update teacher
+const updateTeacher = async (req, res, next) => {
+  const updates = {};
+  const { id } = req.params;
+  const createdBy = req.user.id;
+  const allowedFields = ["title", "description", "image"];
+
+  try {
+    allowedFields.forEach((field) => {
+      if (req.body[field] !== undefined) updates[field] = req.body[field];
+    });
+
+    if (!updates?.title || !updates.title.trim().length) {
+      return res.status(400).json({
+        code: "invalidProperty",
+        message: "Teacher sarlavhasi talab qilinadi",
+      });
+    }
+
+    const updated = await Teacher.findOneAndUpdate(
+      { _id: id, createdBy },
+      updates,
+      { new: true }
+    );
+
+    if (!updated) {
+      return res
+        .status(404)
+        .json({ code: "teacherNotFound", message: "Teacher topilmadi" });
+    }
+
+    res.json({
+      teacher: updated,
+      code: "teacherUpdated",
+      message: "Teacher muvaffaqiyatli yangilandi",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getTeachers,
+  createTeacher,
+  updateTeacher,
+  getTeacherById,
+};

ab321cf8695a946c73f377b185a5ddfd78429607
yaxyobekuz <pubgn9642@gmail.com>
2025-10-05 15:38:31 +0500

    Add routes for uploading and deleting module audios in tests route

--- src/routes/tests.js
+++ src/routes/tests.js
@@ -8,19 +8,32 @@ const {
   updateTest,
   deleteTest,
   getTestById,
-  updateModule,
   getLatestTests,
+  addAudioToModule,
+  updateModuleDuration,
+  deleteAudioFromModule,
 } = require("../controllers/tests.controller");
 
 // Middlewares
 const { auth } = require("../middlewares/auth");
+
+// Multer
+const { upload } = require("../utils/multer.js");
 
 router.get("/", auth, getTests);
 router.post("/", auth, createTest);
 router.put("/:id", auth, updateTest);
 router.delete("/:id", auth, deleteTest);
 router.get("/latest", auth, getLatestTests);
-router.put("/:id/:module", auth, updateModule);
+router.put("/:id/:module/duration", auth, updateModuleDuration);
+router.delete("/:id/:module/audios/:audioId", auth, deleteAudioFromModule);
 router.get("/:id", auth, getTestById);
+
+router.post(
+  "/:id/:module/audios",
+  auth,
+  upload.single("file"),
+  addAudioToModule
+);
 
 module.exports = router;

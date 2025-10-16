4293522ced457386d08ca56d9f64f0343375246a
yaxyobekuz <pubgn9642@gmail.com>
2025-10-16 17:56:13 +0500

    Add authentication routes for resend code, login with code, and sending code to phone

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,6 +8,9 @@ const {
   verify,
   profile,
   register,
+  resendCode,
+  loginWithCode,
+  sendCodeToPhone,
 } = require("../controllers/auth.controller");
 
 // Middlewares
@@ -17,5 +20,8 @@ router.post("/login", login);
 router.post("/verify", verify);
 router.post("/register", register);
 router.get("/profile", auth, profile);
+router.post("/resend-code", resendCode);
+router.post("/login-with-code", loginWithCode);
+router.post("/send-code-to-phone", sendCodeToPhone);
 
 module.exports = router;

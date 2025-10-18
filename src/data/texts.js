a06d70105a31010c557d0c3253df8e5dd910b479
yaxyobekuz <pubgn9642@gmail.com>
2025-10-18 14:19:05 +0500

    Enhance bot message sending: add options parameter to sendMessage function for customizable reply markup.

--- src/data/texts.js
+++ src/data/texts.js
@@ -1,6 +1,26 @@
+const { appendDotZero } = require("../utils/helpers");
+
 const texts = {
   verificationCodeSent: (code) =>
     `<b>Hisobingizni tasdiqlash kodingiz:</b> <code>${code}</code>\nUshbu kodni hech kimga bermang, Hatto IELTS admini so'rasa ham!`,
+  resultReady: (overall, reading, writing, speaking, listening) =>
+    `🎉 <b>Sizning test natijangiz tayyor!</b>\n\n` +
+    `🏆 <b>Umumiy ball:</b> <span class="tg-spoiler">${appendDotZero(
+      overall
+    )}</span>\n\n` +
+    `📖 <b>Reading:</b> <span class="tg-spoiler">${appendDotZero(
+      reading
+    )}</span>\n` +
+    `✍️ <b>Writing:</b> <span class="tg-spoiler">${appendDotZero(
+      writing
+    )}</span>\n` +
+    `🗣 <b>Speaking:</b> <span class="tg-spoiler">${appendDotZero(
+      speaking
+    )}</span>\n` +
+    `🎧 <b>Listening:</b> <span class="tg-spoiler">${appendDotZero(
+      listening
+    )}</span>\n\n` +
+    `Batafsil ma'lumot uchun saytga kiring.`,
 };
 
 module.exports = texts;
--- src/services/bot.js
+++ src/services/bot.js
@@ -3,15 +3,16 @@ const axios = require("axios");
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const BASE_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
 
-const sendMessage = async (chatId, text) => {
+const sendMessage = async (chatId, text, options = {}) => {
   try {
     await axios.post(`${BASE_URL}/sendMessage`, {
       text,
       chat_id: chatId,
       parse_mode: "HTML",
+      reply_markup: options,
     });
     return true;
-  } catch (err) {
+  } catch {
     return false;
   }
 };

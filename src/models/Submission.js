2468b59bf61d6ed44dba239b54aff7ae890d50da
yaxyobekuz <pubgn9642@gmail.com>
2025-10-26 21:45:44 +0500

    refactor: reorganize Submission schema for clarity and consistency

--- src/models/Submission.js
+++ src/models/Submission.js
@@ -3,30 +3,43 @@ const mongoose = require("mongoose");
 const Submission = new mongoose.Schema(
   {
     finishedAt: { type: Date },
-    isScored: { type: Boolean, default: false },
     startedAt: { type: Date, default: Date.now },
-    result: { ref: "Result", type: mongoose.Schema.Types.ObjectId },
     test: { type: mongoose.Schema.Types.ObjectId, ref: "Test", required: true },
     link: { type: mongoose.Schema.Types.ObjectId, ref: "Link", required: true },
+
+    // Result
+    scoredAt: { type: Date, default: null },
+    isScored: { type: Boolean, default: false },
+    scoredBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    result: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Result",
+      default: null,
+    },
+
+    // Answers
     answers: {
       reading: { default: {}, type: Object, required: true },
       writing: { default: {}, type: Object, required: true },
       listening: { default: {}, type: Object, required: true },
     },
     student: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      type: mongoose.Schema.Types.ObjectId,
     },
     teacher: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      type: mongoose.Schema.Types.ObjectId,
     },
     supervisor: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      type: mongoose.Schema.Types.ObjectId,
     },
   },
   { timestamps: true }
